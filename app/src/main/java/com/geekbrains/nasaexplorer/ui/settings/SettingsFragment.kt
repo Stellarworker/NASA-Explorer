0840fe7945095cecd496213b9b5c8f596be99aa9
geekbrains <info@geekbrains.ru>
2022-05-25 00:21:11 +0300

    Lesson 4

--- app/src/main/java/com/geekbrains/nasaexplorer/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/geekbrains/nasaexplorer/ui/settings/SettingsFragment.kt
@@ -1,8 +1,10 @@
 package com.geekbrains.nasaexplorer.ui.settings
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.os.Bundle
 import android.view.View
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.Fragment
 import com.geekbrains.nasaexplorer.R
 import com.geekbrains.nasaexplorer.common.EMPTY_INT
@@ -12,6 +14,9 @@ import com.geekbrains.nasaexplorer.utils.convertRBIDtoTID
 import com.geekbrains.nasaexplorer.utils.convertTIDtoRBID
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    private val INITIAL_SIZE = 0f
+    private val DURATION = 500L
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -25,15 +30,47 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
                     )
                 )
                 setOnCheckedChangeListener { _, checkedId ->
+                    binding.animationRadiogroup1.transitionToEnd()
                     activity.getPreferences(Context.MODE_PRIVATE)
                         .edit()
                         .putInt(THEME_ID, convertRBIDtoTID(checkedId))
                         .apply()
                     activity.setTheme(checkedId)
-                    activity.recreate()
                 }
             }
         }
 
+        val transitionListener = object : MotionLayout.TransitionListener {
+
+            override fun onTransitionStarted(p0: MotionLayout?, startId: Int, endId: Int) {}
+
+            override fun onTransitionChange(
+                p0: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, currentId: Int) {
+                activity?.recreate()
+            }
+
+            override fun onTransitionTrigger(
+                p0: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+            }
+        }
+        binding.animationRadiogroup1.setTransitionListener(transitionListener)
+        ValueAnimator.ofFloat(INITIAL_SIZE, resources.getDimension(R.dimen.common_header_small))
+            .apply {
+                duration = DURATION
+                addUpdateListener {
+                    binding.settingsFragmentThemesHeader.textSize = it.animatedValue as Float
+                }
+            }.start()
     }
 }
